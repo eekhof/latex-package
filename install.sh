bfc2bc1fd909dd5bf09b72374ab0278a198bcdfa
Lucas Eekhof <LucasEekhof@gmx.de>
2023-01-03 15:18:36 +0100

    Added script to automatically compile and install on laptop

---
+++ install.sh
@@ -0,0 +1,11 @@
+# This script is meant to run on lueelap
+
+cd ./LaTeX-Workshop/dev # Clone git repo from https://github.com/James-Yu/LaTeX-Workshop
+
+python pkgcommand.py -i ../../eekhof.cwl -o ../../
+
+cd ..
+cd ..
+
+cp eekhof.sty ~/texmf/tex/latex/eekhof.sty
+cp eekhof.json ~/texmf/cwl/eekhof.json
