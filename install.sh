39f44580a33fe23a9f8630a270f281b4e71442b9
Lucas Eekhof <LucasEekhof@gmx.de>
2023-01-15 14:39:02 +0100

    Set to use proper arial font in cheatsheet mode

--- install.sh
+++ install.sh
@@ -10,3 +10,6 @@ cd ..
 
 cp eekhof.sty ~/texmf/tex/latex/eekhof.sty
 cp eekhof.json ~/texmf/cwl/eekhof.json
+
+echo "Successfully installed. Remember to change the First- and Surname, as well as the Matrikelnummer in ~/texmf/tex/latex/eekhof.sty . The directory will be opened now."
+xdg-open ~/texmf/tex/latex/eekhof.sty
