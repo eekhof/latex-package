9b11783a43f55b5a8c9d4dbe78d55053c32aecf7
Lucas Eekhof <LucasEekhof@gmx.de>
2023-03-09 21:29:28 +0100

    Added prompt for personal parameters during installation; Added check if texhash needs to be run and auto-running it if neccessary

--- install.sh
+++ install.sh
@@ -3,13 +3,36 @@
 
 cd ./LaTeX-Workshop/dev # Clone git repo from https://github.com/James-Yu/LaTeX-Workshop
 
-python pkgcommand.py -i ../../eekhof.cwl -o ../../
+python pkgcommand.py -i ../../eekhof.cwl -o ../../ # Generate JSON for VSCode-Completion from provided CWL-file #TODO SEEMS to output some code, could be tried to be suppressed with &>/dev/null , but try if script then still executes properly
 
 cd ..
 cd ..
 
-cp eekhof.sty ~/texmf/tex/latex/eekhof.sty
+echo What is your first name?
+read firstname
+echo What is your surname?
+read surname
+echo What is your Matrikelnummer?
+read matrikelnummer
+
+# Test if file exists already, i. e. if texhash-command needs to be run after installation:
+if test -f "$HOME/texmf/tex/latex/eekhof.sty"; then
+    newinstall=false
+else
+    newinstall=true
+fi
+
+
+sed -e "s/FIRSTNAMEHERE/$firstname/g" -e "s/SURNAMEHERE/$surname/g" -e "s/MATRIKELNUMMERHERE/$matrikelnummer/g" eekhof.sty > ~/texmf/tex/latex/eekhof.sty # Installs package to proper place after inserting personal parameters
 cp eekhof.json ~/texmf/cwl/eekhof.json
 
-echo "Successfully installed. Remember to change the First- and Surname, as well as the Matrikelnummer in ~/texmf/tex/latex/eekhof.sty . The directory will be opened now."
-xdg-open ~/texmf/tex/latex/eekhof.sty
+
+if $newinstall ; then
+    echo "This seems to be the first install of this package, running texhash..."
+    texhash
+else
+    echo "A version package has already been installed once, no need to run texhash."
+fi
+echo "Thank you $firstname $surname. The Package has been successfully installed."
+
+# xdg-open ~/texmf/tex/latex/eekhof.sty # Opens installed package in preferred text editor
